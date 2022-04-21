b5b9bb89fbb200ddd6ee30a5cebf7d97eddbb2de
Roman Tereshkov <tereshkov291993@gmail.com>
2022-04-21 18:20:34 +0300

    Мобильная версия

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container : {
+      center: true
+    },
+    extend: {
+      colors: {
+        gold: '#D8B152',
+        gray: '#AEA38B',
+        lightblack: '#202020',
+        tblack: '#424242'
+      }
+    },
+  },
+  plugins: [],
+}
